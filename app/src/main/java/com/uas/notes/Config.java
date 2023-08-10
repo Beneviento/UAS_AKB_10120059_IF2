f7b2b1264311aae165bb188156b523ef98988bf5
Fazrinn <74952309+Beneviento@users.noreply.github.com>
2023-08-10 23:17:33 +0700

    Initial commit

---
+++ app/src/main/java/com/uas/notes/Config.java
@@ -0,0 +1,20 @@
+/**
+ * NIM : 10120059
+ * NAMA : Muhammad Fazrin Ar-Rahman
+ * KELAS : IF-2
+ */
+package com.uas.notes;
+
+public class Config {
+
+    public static String DB_URL = "https://uas-10120059-default-rtdb.firebaseio.com/";
+    public static String FCM_SERVER_KEY = "AAAA1Eu7Zq4:APA91bGar7pfZO67VRPi4m_fcRHYW46qqplGezBetgQ31n-T9KSnRhyFKs6527v-VuWH-UEZmRcB-F83PZaS1zVKmWyhi12L81bzTl1NZsVnj0k4W62paE_uTGnoXv34gPjSB35SJapp";
+
+    public static String getDB_URL() {
+        return DB_URL;
+    }
+
+    public static String getFCM_SERVER_KEY() {
+        return FCM_SERVER_KEY;
+    }
+}
---
+++ app/src/main/java/com/uas/notes/model/NoteCategory.java
@@ -0,0 +1,10 @@
+/**
+ * NIM : 10120059
+ * NAMA : Muhammad Fazrin Ar-Rahman
+ * KELAS : IF-2
+ */
+package com.uas.notes.model;
+
+public class NoteCategory {
+    public String title, total;
+}
